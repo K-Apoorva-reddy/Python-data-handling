bfa3a34facff1e3fbc10baab7c27d2153bed25e1
Apoorva reddy <kuntaapoorva62@gmail.com>
2026-07-01 19:59:12 +0530

    Save cleaned data to cleaned_employees.csv
    Add functionality to save cleaned data to a CSV file.

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ df = df.drop_duplicates()
 print("\nCleaned Data:")
 print(df)
 
+df.to_csv("data/cleaned_employees.csv", index=False)
+print("\nCleaned data saved successfully as cleaned_employees.csv")
+
 # Statistics
 print("\nAverage Salary:", df["Salary"].mean())
 print
